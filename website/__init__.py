db37d17ac92a782bebb111a174ede3e81ab48aeb
unknown <kawshikbss@gmail.com>
2021-04-06 02:26:35 +0600

    Created flask app inside package

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 def create_app():
+    # creating Flask app
     app = Flask(__name__)
     # confiuring app secret key
     app.config['SECRET_KEY'] = "Lucifer Morningstar"

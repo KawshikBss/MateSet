c6aedb22d50968995fccbd81a90c6954f865db61
unknown <kawshikbss@gmail.com>
2021-04-06 02:22:17 +0600

    Created website dir

---
+++ website/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    # confiuring app secret key
+    app.config['SECRET_KEY'] = "Lucifer Morningstar"
+
+    # returning app
+    return app

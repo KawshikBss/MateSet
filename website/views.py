174379aadd9a8b817d646a11019ee157b30cd9a8
unknown <kawshikbss@gmail.com>
2021-04-06 19:43:45 +0600

    defined route to homepage in views

--- website/views.py
+++ website/views.py
@@ -2,3 +2,9 @@ from flask import Blueprint
 
 # defining blueprint
 views = Blueprint("views", __name__)
+
+# defining route to homepage
+@views.route("/")
+@views.route("/home")
+def home():
+    return "<h1>Home</h1>"

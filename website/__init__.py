a84f7a4e75a80a000b3f75c8a9109e85301f9b59
unknown <kawshikbss@gmail.com>
2021-04-06 20:05:39 +0600

    registered blueprint of views in app

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Blueprint
 
 def create_app():
     """To crate a Flask app
@@ -10,6 +11,10 @@ def create_app():
     app = Flask(__name__)
     # confiuring app secret key
     app.config['SECRET_KEY'] = "Lucifer Morningstar"
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
 
     # returning app
     return app

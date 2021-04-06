63f11c971fe8fb8713626b4339d99bd3725eac55
unknown <kawshikbss@gmail.com>
2021-04-06 13:55:20 +0600

    updated __init__

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
 def create_app():
+    """To crate a Flask app
+
+    Returns:
+        app: a flask app
+    """    
     # creating Flask app
     app = Flask(__name__)
     # confiuring app secret key

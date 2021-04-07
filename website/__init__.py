57f5124961a1c6d4e3c4c6ec2e25355afdbc471a
unknown <kawshikbss@gmail.com>
2021-04-07 21:38:09 +0600

    created and configured database object db

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask
-from flask import Blueprint
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+# creating database object
+bd = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     """To crate a Flask app
@@ -9,8 +13,13 @@ def create_app():
     """    
     # creating Flask app
     app = Flask(__name__)
+
     # confiuring app secret key
     app.config['SECRET_KEY'] = "Lucifer Morningstar"
+
+    # configuring sqlite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth

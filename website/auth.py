048c8f3ad69ee65237d6cc7eac065bf00cc77232
unknown <kawshikbss@gmail.com>
2021-04-07 19:20:31 +0600

    added get and post methods and data retrive from from

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # defining blueprint
 auth = Blueprint("auth", __name__)
 
 # defining route to login
-@auth.route("/login/")
+@auth.route("/login/", methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print("Username = ", username, " Password = ", password)
     return render_template("login.html")

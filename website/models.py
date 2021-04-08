1b17f1bc6f3f3ffa08ada01e3705e911083b0c74
unknown <kawshikbss@gmail.com>
2021-04-09 03:38:01 +0600

    created Like db model and added likes attribute in Post to count

--- website/models.py
+++ website/models.py
@@ -10,8 +10,16 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     posts = db.relationship('Post')
+    liked = db.relationship('Like')
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     desc = db.Column(db.String(100))
     userName = db.Column(db.String(100), ForeignKey('user.userName'))
+    likes = db.Column(db.Integer, default=0)
+    liked = db.relationship('Like')
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post = db.Column(db.Integer, ForeignKey('post.id'))
+    user = db.Column(db.String(100), ForeignKey('user.userName'))

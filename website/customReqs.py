402102babd7bf1b7752ce8a065ba1ddd4ccd5b8e
unknown <kawshikbss@gmail.com>
2021-04-09 17:03:54 +0600

    created blueprint thats sends custom post requests

---
+++ website/customReqs.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, json
+from flask_login import current_user
+from .models import User, Post, Like
+from . import db
+
+customReqs = Blueprint('customReqs', __name__)
+
+@customReqs.route('/like-post', methods=['POST'])
+def like_post():
+    post = json.loads(request.data)['postId']
+    tmpPost = Post.query.filter_by(id=post).first()
+    ifUser = [like for like in tmpPost.liked if like.user == current_user.userName]
+    if ifUser:
+        tmpPost.likes -= 1
+        db.session.delete(ifUser[0])
+    else:
+        tmpPost.likes += 1
+        db.session.add(Like(post=post, user=current_user.userName))
+    db.session.commit()
+
+    return jsonify({})

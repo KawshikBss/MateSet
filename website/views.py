a2da219088d358f7fd8208f31ccb6f35dae4e1b1
unknown <kawshikbss@gmail.com>
2021-04-12 22:53:53 +0600

    fixed suggestions in following view

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from .models import User, Post
 from . import db
+from .getFromModels import *
 #from file import send_image
 
 # defining blueprint
@@ -16,24 +17,19 @@ def home():
         desc = request.form['post']
         db.session.add(Post(desc=desc, userName=current_user.userName))
         db.session.commit()
-    posts = Post.query.all()
-    print(current_user.following)
-    followedUsers = [usr.followedUser for usr in current_user.following]
-    print(followedUsers)
-    posts = [post for post in posts if post.userName in followedUsers or post.userName == current_user.userName]
-    suggestions = [sug for sug in User.query.all() if not sug.id == current_user.id and sug.userName not in followedUsers]
+    posts = get_posts_for_user(current_user)
+    suggestions = get_users_suggestions(current_user)
     return render_template('home.html', user=current_user, posts=posts, sugs=suggestions)
 
 @views.route('/<username>/')
 @login_required
 def profile(username):
-    suggestions = [sug for sug in User.query.all() if not sug.id == current_user.id]
+    suggestions = get_users_suggestions(current_user)
     return render_template('profile.html', user=current_user, sugs=suggestions)
 
 @views.route('/following/')
 @login_required
 def following():
-    userFollowing = [usr.followedUser for usr in current_user.following]
-    users = User.query.all()
-    usersFollowing = [usr for usr in users if usr.userName in userFollowing]
-    return render_template('following.html', user=current_user, usersFollowing=usersFollowing)
+    usersFollowing = get_users_following(current_user)
+    suggestions = get_users_suggestions(current_user)
+    return render_template('following.html', user=current_user, usersFollowing=usersFollowing, sugs=suggestions)

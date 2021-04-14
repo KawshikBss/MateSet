906c8a3e870a3051a71ec3d47f5b2d0bf95d2d8f
unknown <kawshikbss@gmail.com>
2021-04-14 14:36:13 +0600

    defined settngs route

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # defining blueprint
@@ -69,3 +69,58 @@ def signup():
                 flash("Username is taken")
 
     return render_template('signup.html')
+
+@auth.route('/settings/', methods=['POST', 'GET'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        currPassword = request.form['curpassword']
+        if not currPassword:
+            flash("Must confirm current password to save changes")
+        else:
+            if check_password_hash(current_user.password, currPassword):
+                newUserName = request.form['username']
+                if newUserName:
+                    if len(newUserName) < 2:
+                        flash("Username is too short")
+                    else:
+                        if newUserName == current_user.userName:
+                            flash("You are already using this user name")
+                        else:
+                            alreadyExists = User.query.filter_by(userName=newUserName).first()
+                            if alreadyExists:
+                                flash("User name already exists")
+                            else:
+                                current_user.userName = newUserName
+                                flash(f"User name changed to {newUserName}")
+                newName = request.form['name']
+                if newName:
+                    if ' ' not in newName:
+                        flash("First name and Last name  ust be included in name")
+                    else:
+                        current_user.name = newName
+                        flash(f"Name changed to {newName}")
+                newEmail = request.form['email']
+                if newEmail:
+                    if '@' in newEmail and '.com' in newEmail:
+                        emailAlreadyExists = User.query.filter_by(email=newEmail).first()
+                        if emailAlreadyExists:
+                            flash("Email already exists")
+                        else:
+                            current_user.email = newEmail
+                            flash("Email changed")
+                newPassword = request.form['password']
+                if newPassword:
+                    if len(newPassword) < 8:
+                        flash("Password must be atleast 8 characters")
+                    else:
+                        if check_password_hash(current_user.password, newPassword):
+                            flash("This is the current password")
+                        else:
+                            current_user.password = generate_password_hash(newPassword, method='sha256')
+                            flash("Password changed")
+                db.session.commit()
+            else:
+                flash("Current password doesn't match")
+        
+    return render_template('settings.html', user=current_user)
